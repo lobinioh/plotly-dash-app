cc7ad5b6f770397db31f26713a92b07670cf4edf
lobinioh <info@wm-studio78.de>
2026-03-19 15:55:22 +0100

    Update PlotIterator.py

--- PlotIterator.py
+++ PlotIterator.py
@@ -1,14 +1,20 @@
-# app.py
-import dash
-from dash import html
-from PlotIterator import PlotIterator
+class PlotIterator:
+    def __init__(self, data=None):
+        self.data = data if data is not None else [1, 2, 3, 4, 5]
+        self.index = 0
 
-# Dash App erstellen
-app = dash.Dash(__name__)
-pi = PlotIterator()
+    def __iter__(self):
+        return self
 
-# Minimal Layout
-app.layout = html.Div([
-    html.H1("Minimal Dash App mit PlotIterator"),
-    html.Ul([html.Li(str(val)) for val in pi.data])
-])
+    def __next__(self):
+        if self.index >= len(self.data):
+            raise StopIteration
+        value = self.data[self.index]
+        self.index += 1
+        return value
+
+# Nur Testcode hier, importiert nicht sich selbst!
+if __name__ == "__main__":
+    pi = PlotIterator()
+    for val in pi:
+        print(val)
